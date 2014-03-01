4eb53f608cd8c62197d4df61a65a5c624ec00746
Xiaoqi Liu <xiaoqiliu7@gmail.com>
2014-03-01 17:16:54 -0500

    Initial Commit

---
+++ SpriteKitPhysicsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitPhysicsTest
+//
+//  Created by Xiaoqi Liu on 3/1/14.
+//  Copyright (c) 2014 Xiaoqi Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitPhysicsTest/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitPhysicsTest
+//
+
+//  Copyright (c) 2014 Xiaoqi Liu. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
